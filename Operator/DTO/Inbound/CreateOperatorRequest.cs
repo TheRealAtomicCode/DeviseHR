e57b69d5abece4f13e56ba72e42ddf036c1cb0ac
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-10-26 22:36:30 +0200

    adjusted the ReateOperatorRequest DTO - TEST

---
+++ Operator/DTO/Inbound/CreateOperatorRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace OP.DTO.Inbound
+{
+    public class CreateOperatorRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string? PasswordHash { get; set; } = string.Empty;
+
+        public int UserRole { get; set; }
+    }
+}
