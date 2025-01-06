d5f45ed1a2c41a5bce3aa5a86226ff31849afa6f
Atomic Code <81088443+TheRealAtomicCode@users.noreply.github.com>
2025-01-06 20:51:37 +0200

    made the side nav responsive to the bottom of the screen on mobile

--- HR/HR_Client/src/Layouts/AuthenticatedLayout.tsx
+++ HR/HR_Client/src/Layouts/AuthenticatedLayout.tsx
@@ -1,16 +1,15 @@
-import { Outlet } from 'react-router-dom'
-import SideNav from '../Components/Nav/SideNav' 
+import { Outlet } from 'react-router-dom';
+import SideNav from '../Components/Nav/SideNav';
 
-function AuthenticatedLayout(){
-    return (
-      <div className='ml-16'>
-        <SideNav /> {/* This is the navigation bar */}
-        <main>
-          <Outlet /> {/* Nested route content will be rendered here */}
-        </main>
-      </div>
-    );
-  };
+function AuthenticatedLayout() {
+	return (
+		<div className="sm:ml-16">
+			<SideNav /> {/* This is the navigation bar */}
+			<main>
+				<Outlet /> {/* Nested route content will be rendered here */}
+			</main>
+		</div>
+	);
+}
 
-  export default AuthenticatedLayout;
-
+export default AuthenticatedLayout;
--- HR/HR_Client/src/components/Nav/SideNav.tsx
+++ HR/HR_Client/src/components/Nav/SideNav.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
-import { FaFire } from 'react-icons/fa';
+import { FaRegClock } from 'react-icons/fa';
 import { IoHomeOutline } from 'react-icons/io5';
 import { PiUsersThreeLight } from 'react-icons/pi';
-import { CiUser } from 'react-icons/ci';
 import { IoCalendarOutline } from 'react-icons/io5';
 import { TiFolderOpen } from 'react-icons/ti';
 import { GoGear } from 'react-icons/go';
+import { FiPlus } from 'react-icons/fi'; // For the plus icon
+import { GiHamburgerMenu } from 'react-icons/gi'; // For the burger menu
 
 // Define the type for the SideBarIcon props
 interface SideBarIconProps {
@@ -15,43 +16,75 @@ interface SideBarIconProps {
 
 function SideNav() {
 	return (
-		<div className="side-nav fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg overflow-y-auto overflow-x-hidden scrollbar-hidden">
-			<i>
-				<div className="cursor-pointer w-14 h-14 mt-4 rounded-full border border-white items-center justify-center mx-auto"></div>
-			</i>
-			<i className="mt-12">
-				<SideBarIcon icon={<IoHomeOutline size="28" />} title={'Home'} />
-			</i>
-			<i>
-				<SideBarIcon icon={<PiUsersThreeLight size="28" />} title={'Users'} />
-			</i>
-			<i>
-				<SideBarIcon icon={<CiUser size="28" />} title={'Profile'} />
-			</i>
-			<i>
-				<SideBarIcon
-					icon={<IoCalendarOutline size="28" />}
-					title={'Calendar'}
-				/>
-			</i>
-			<i>
-				<SideBarIcon icon={<TiFolderOpen size="28" />} title={'Files'} />
-			</i>
-			<i className="mt-auto mb-6">
-				<SideBarIcon icon={<GoGear size="28" />} />
-			</i>
+		<div>
+			{/* Sidebar for larger screens */}
+			<div className="side-nav hidden sm:flex fixed top-0 left-0 h-screen w-16 m-0 flex-col bg-gray-900 text-white shadow-lg overflow-y-auto overflow-x-hidden scrollbar-hidden">
+				<i>
+					<div className="cursor-pointer w-14 h-14 mt-4 rounded-full border border-white items-center justify-center mx-auto"></div>
+				</i>
+				<i className="mt-12">
+					<MdSideBarIcon icon={<IoHomeOutline size="28" />} title={'Home'} />
+				</i>
+				<i>
+					<MdSideBarIcon
+						icon={<PiUsersThreeLight size="28" />}
+						title={'Employees'}
+					/>
+				</i>
+				<i>
+					<MdSideBarIcon
+						icon={<IoCalendarOutline size="28" />}
+						title={'Calendar'}
+					/>
+				</i>
+				<i>
+					<MdSideBarIcon icon={<FaRegClock size="28" />} title={'Rotas'} />
+				</i>
+				<i>
+					<MdSideBarIcon icon={<TiFolderOpen size="28" />} title={'Files'} />
+				</i>
+				<i className="mt-auto mb-6">
+					<MdSideBarIcon icon={<GoGear size="28" />} />
+				</i>
+			</div>
+
+			{/* Bottom bar for mobile screens */}
+			<div className="sm:hidden fixed bottom-0 left-0 w-full bg-gray-900 text-white flex justify-between items-center p-2">
+				<SmSideBarIcon icon={<IoHomeOutline size="28" />} />
+				<SmSideBarIcon icon={<FaRegClock size="28" />} />
+				<SmSideBarIcon icon={<FiPlus size="28" />} />
+				<SmSideBarIcon icon={<IoCalendarOutline size="28" />} />
+				<SmSideBarIcon icon={<GiHamburgerMenu size="28" />} />
+			</div>
 		</div>
 	);
 }
 
-const SideBarIcon: FC<SideBarIconProps> = ({ icon, title }) => {
+const MdSideBarIcon: FC<SideBarIconProps> = ({ icon, title }) => {
 	return (
 		<div className="hover:text-green-400 cursor-pointer group">
 			<div className="relative flex items-center justify-center h-12 w-12 mt-2 mb-[2px] mx-auto bg-gray-800 rounded-lg group-hover:rounded-3xl transition-all duration-200">
 				{icon}
 			</div>
+			{/* Hide text on small screens */}
 			{title && (
-				<p className="mx-auto text-white text-xs mt-1 group-hover:text-green-400 text-center">
+				<p className="mx-auto text-white text-xs mt-1 group-hover:text-green-400 text-center hidden sm:block">
+					{title}
+				</p>
+			)}
+		</div>
+	);
+};
+
+const SmSideBarIcon: FC<SideBarIconProps> = ({ icon, title }) => {
+	return (
+		<div className="hover:text-green-400 cursor-pointer group">
+			<div className="relative flex items-center justify-center h-8 w-12  mb-[2px] mx-auto  rounded-lg group-hover:rounded-3xl transition-all duration-200">
+				{icon}
+			</div>
+			{/* Hide text on small screens */}
+			{title && (
+				<p className="mx-auto text-white text-xs mt-1 group-hover:text-green-400 text-center hidden sm:block">
 					{title}
 				</p>
 			)}
